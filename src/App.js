15803fe5c63d1427993a5fb70193fb51691b6708
Aleksandra <78027205+Aleksandra-code@users.noreply.github.com>
2021-07-08 23:07:47 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+     <h1>Weather App</h1> 
+      <footer>
+        This project is coded by Aleksandra Staszynska and is{" "} 
+      <a href="https://github.com/Aleksandra-code/react-weather-app"
+      target="blank">
+        open-sourced on GitHub
+      </a>
+      </footer>
+      </div>
   );
 }
 
